81843f69cc1c3dd6378ac67b533258ad5724eeea
chaso13 <charlesrheavenr@gmail.com>
2019-05-22 02:23:50 -0400

    changing the navbar styling to meet client specs

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -9,43 +9,43 @@ class Nvbr extends Component {
     render () {
         return (
             <div>
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar className="Navbar" collapseOnSelect expand="lg" bg="dark" variant="dark" ></Navbar>
+                <Navbar className="Navbar" collapseOnSelect expand="lg" bg="light" variant="light" >
                     <Navbar.Brand href="#home">
                         <img className="bologna" src={logo} alt=" " />
                         <img className="bologna" src={logo2} alt=" " />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mr-auto">
-                        <Nav.Link href="#features">In Your Community</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                        <Nav className="ml-auto">
+                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                         </NavDropdown>
-                        </Nav>
-                        <Nav>
-                        <Nav.Link href="#deets">More deets</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                    <Dropdown className="button">
-                        <Dropdown.Toggle className="button">
-                            Location Selector
-                        </Dropdown.Toggle>
+                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#features">In Your Community</Nav.Link>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Albany</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Savannah</Dropdown.Item>
-                            <Dropdown.Item href="#/action-3">Atlanta</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Location className="button"/>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        
+                    </Navbar.Collapse>
+                    
                 </Navbar>
             </div>
         )
